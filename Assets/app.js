31e6aca46d48badbf57728ed20fd738753fff12d
Fernando Rodrigues <nandocf20@gmail.com>
2022-12-08 03:05:50 -0300

    dark and light mode button  inluded

--- Assets/app.js
+++ Assets/app.js
@@ -1,6 +1,10 @@
+const body = document.querySelector('body')
 const clockDiv = document.querySelector('.clock-container')
 const dayDiv = document.querySelector('.day-container')
+const mainContainer = document.querySelector('.main-container')
+const darkLightMode = document.querySelector('#darkLight-mode')
 
+// função mostra as horas
 const currentHour = () => {
   const completeDate = new Date()
   const hours = completeDate.getHours()
@@ -14,10 +18,12 @@ const currentHour = () => {
   `
   clockDiv.innerHTML = showHours
 }
+currentHour()//Para que a aplicação já comece executando a hora antes do setInterval.
 
 const refreshHours = setInterval(currentHour, 1000)
 
 
+// Função mostra a data
 const currentDay = () => {
   const completeDate2 = new Date()
   const day = completeDate2.getDate()
@@ -34,3 +40,30 @@ const currentDay = () => {
 }
 
 currentDay()
+
+// Função botão light/dark mode
+darkLightMode.addEventListener('click', event => {
+  const clickedElement = event.target
+
+//primeiro if dark para light
+  if (clickedElement.className === 'dark-mode-active') {
+    clickedElement.setAttribute('class', 'light-mode-active')
+    clickedElement.innerText = 'dark mode'
+
+    body.setAttribute('class', 'body-light')
+    mainContainer.setAttribute('class', 'main-container-light')
+    clockDiv.setAttribute('class', 'clock-container-light')
+    dayDiv.setAttribute('class', 'day-container-light')
+
+//else if light para dark
+  } else if (clickedElement.className === 'light-mode-active') {
+    clickedElement.setAttribute('class', 'dark-mode-active')
+    clickedElement.innerText = 'light mode'
+
+    body.removeAttribute('class', 'body-light')
+    mainContainer.setAttribute('class', 'main-container')
+    clockDiv.setAttribute('class', 'clock-container')
+    dayDiv.setAttribute('class', 'day-container')
+
+  }
+})
